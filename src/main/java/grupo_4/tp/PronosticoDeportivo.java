425b040e783dbca2ef4fe8bdd7927896615c2819
Juan <jprivas7@gmail.com>
2023-04-16 10:11:10 -0300

    cargaDeDB() pronosticos y participantes ok

--- src/main/java/grupo_4/tp/PronosticoDeportivo.java
+++ src/main/java/grupo_4/tp/PronosticoDeportivo.java
@@ -13,14 +13,14 @@ import java.util.List;
 public class PronosticoDeportivo {
     private ListaEquipos equipos;
     private ListaPartidos partidos;
-    /*private ListaPronosticos pronosticos;
-    private ListaParticipantes participantes;*/
+    private ListaPronosticos pronosticos;
+    private ListaParticipantes participantes;
 
     public PronosticoDeportivo() {
         equipos = new ListaEquipos();
         partidos = new ListaPartidos();
-        /*pronosticos = new ListaPronosticos();
-        participantes = new ListaParticipantes();*/
+        pronosticos = new ListaPronosticos();
+        participantes = new ListaParticipantes();
     }
 
     public void play(){
@@ -30,13 +30,14 @@ public class PronosticoDeportivo {
         // cargar lista de partidos
         partidos.cargarDeDB(equipos);
         System.out.println("Los partidos cargados son: " + partidos.listar()+ "\n" );
-       /* // cargar lista de pronosticos
-        pronosticos.cargarDeArchivo(partidos);
+        // cargar lista de pronosticos
+        pronosticos.cargarDeDB(partidos);
         System.out.println("Los pronosticos cargados son: " + pronosticos.listar()+ "\n" );
         
         //cargar la lista de participantes con su lista de pronostico de cada participante
-        participantes.cargarDeArchivo();       
-        String lista = "";
+        participantes.cargarDeDB();
+        System.out.println("Los participantes cargados son: " + participantes.listar()+ "\n" );
+      /*  String lista = "";
         String aciertosParticipante = ""; // carga la lista de aciertos y puntaje de cada participante
         List<Participante> p = participantes.getParticipantes();
         for (Participante participante: p ) {
